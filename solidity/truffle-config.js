26c254b6b240a199ede127f5fcf0d06cb4ea2ae4
DimitrK <jim.feedback@gmail.com>
2021-12-13 04:32:08 +0200

    use eth gas estimation on tests

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -11,37 +11,31 @@ const mnemonic = process.env.HDWALLET_MNEMONIC;
 module.exports = {
   compilers: {
     solc: {
-      version: '0.8.9',
-    },
+      version: '0.8.9'
+    }
   },
   contracts_build_directory: path.join(__dirname, '../client/app/contracts'),
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
   },
   solc: {
-      optimizer: {
-          enabled: true,
-          runs: 200
-      }
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
   },
   networks: {
     ropsten: {
-      provider: new HDWalletProvider(
-        mnemonic,
-        process.env.ROPSTEN_PROVIDER_URL,
-      ),
+      provider: new HDWalletProvider(mnemonic, process.env.ROPSTEN_PROVIDER_URL),
       network_id: 3,
       gas: 4612388,
-      gasPrice: 100000000000,
+      gasPrice: 100000000000
     },
     rinkeby: {
-      provider: new HDWalletProvider(
-        mnemonic,
-        process.env.RINKEBY_PROVIDER_URL,
-      ),
+      provider: new HDWalletProvider(mnemonic, process.env.RINKEBY_PROVIDER_URL),
       network_id: 4,
       gas: 4612388,
-      gasPrice: 100000000000,
+      gasPrice: 100000000000
     },
     development: {
       host: 'localhost',
@@ -49,6 +43,15 @@ module.exports = {
       network_id: 1337,
       gasPrice: 134000000000,
       gas: 4612388
-    },
+    }
   },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD',
+      gasPriceApi: process.env.ETHERSCAN_API_KEY
+        ? `https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=${process.env.ETHERSCAN_API_KEY}`
+        : `https://api.etherscan.io/api?module=proxy&action=eth_gasPrice`,
+    },
+  }
 };
